94495de9003708fe5f167ac2cfba942ff105e204
khanhlecodegym <44794251+khanhlecodegym@users.noreply.github.com>
2020-04-23 15:49:09 +0700

    09-lesson

--- resources/views/dashboard/customers.blade.php
+++ resources/views/dashboard/customers.blade.php
@@ -5,19 +5,21 @@
 
     <form action="customers" method="POST" class="pb-5">
         <div class="input-group">
-            <input type="text" name="name">
-            @error('name')
+            <input type="text" name="name" value="{{ old('name') }}">
             <p class="text-warning">{{ $errors->first('name') }}</p>
-            @enderror
-            <button type="submit">Add Customer</button>
-
-            @csrf
         </div>
+        <div class="input-group">
+            <input type="text" name="email" value="{{ old('email') }}">
+            <p class="text-warning">{{ $errors->first('email') }}</p>
+        </div>
+        <button type="submit">Add Customer</button>
+
+         @csrf
     </form>
 
     <ul>
         @foreach ($customers as $customer)
-            <li>{{ $customer->name }}</li>
+            <li>{{ $customer->name }} <span class="text-muted">({{ $customer->email }})</span></li>
         @endforeach
     </ul>
 @endsection
