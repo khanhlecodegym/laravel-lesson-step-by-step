a64969ef5cb6ef4c55735e3b78f12af5a017c6ce
khanhlecodegym <44794251+khanhlecodegym@users.noreply.github.com>
2020-04-23 14:37:45 +0700

    01-lesson

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('contact', function () {
+    return 'Contact CodeGymer';
+});
+
+Route::get('about', function () {
+    return 'About CodeGymer';
+});
